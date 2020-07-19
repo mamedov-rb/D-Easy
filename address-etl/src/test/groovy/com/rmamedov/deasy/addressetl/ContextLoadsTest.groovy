f119c5bd95e9e8eff38bd664c5ef505b64b939c0
Мамедов Рустам <Rustam.Mamedov@excelian.com>
2020-07-19 10:52:59 +0300

    Added context loads tests for ETl.

---
+++ address-etl/src/test/groovy/com/rmamedov/deasy/addressetl/ContextLoadsTest.groovy
@@ -0,0 +1,19 @@
+package com.rmamedov.deasy.addressetl
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import spock.lang.Specification
+
+@SpringBootTest(classes = AddressEtlApplication.class)
+class ContextLoadsTest extends Specification {
+
+    @Autowired
+    private ApplicationContext context
+
+    def "Application context starts"() {
+        expect:
+        context
+    }
+
+}
