43395bee8a62d5e13827ea4e77b80e433a20d5af
Мамедов Рустам <Rustam.Mamedov@excelian.com>
2020-07-19 11:49:59 +0300

    Added context loads tests for: - all ETl. - OrderService.

--- order-service/src/main/java/com/rmamedov/deasy/orderservice/config/topic/KafkaTopicCreateConfig.java
+++ order-service/src/main/java/com/rmamedov/deasy/orderservice/config/topic/KafkaTopicCreateConfig.java
@@ -6,8 +6,10 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 
+@Profile("!TEST")
 @Configuration
 @RequiredArgsConstructor
 public class KafkaTopicCreateConfig {
